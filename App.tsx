fff1f8f1fbad9d607e3eaaa7b968ba254ca84206
azadsarxanli <azad.sarxanli@gmail.com>
2023-09-05 15:48:08 +0200

    init test

--- App.tsx
+++ App.tsx
@@ -7,14 +7,7 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
 
 import {
   Colors,
